f1bc05f2b0cb3b7332296227de78a907f86a9080
dinglibo <dinglibo@inmyshow.com>
2023-03-08 15:52:49 +0800

    Initial commit

---
+++ schema-init/src/main/java/com/sun/Application.java
@@ -0,0 +1,16 @@
+package com.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author:xxxxx
+ * @create: 2023-03-08 15:33
+ * @Description: 主类
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
